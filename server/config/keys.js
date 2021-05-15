7ad813085e013619bafcf8744b63169bef50afae
Hlanga <henry.bani.hb@gmail.com>
2021-05-15 18:03:53 +0200

    step1OAuthWorks
    gets to google profile

---
+++ server/config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    googleClientID: '937419476142-dj0eth16uqocqu16gvp787ihf52l3sf7.apps.googleusercontent.com',
+    googleClientSectret: 'RziPbeVuC_EvkiXmaRnYDDVo'
+};
--- server/index.js
+++ server/index.js
@@ -1,16 +1,28 @@
 const express = require('express');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
+
 const app = express();
 
-<<<<<<< Updated upstream
-app.get('/', (req, res) =>{
-    res.send({hi:'there'})
-});
-=======
-passport.use(new GoogleStrategy());
+passport.use(
+    new GoogleStrategy(
+        {
+        clientID: keys.googleClientID,
+        clientSecret: keys.googleClientSecret,
+        callbackURL:'/auth/google/callback'
+        },
+        accessToken => {
+            console.log(accessToken);
+        }
+    ));
 
-
->>>>>>> Stashed changes
+app.get('/auth/google', 
+    passport.authenticate('google',
+        { scope:['profile','email']})
+    );
+    
+    
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
+
