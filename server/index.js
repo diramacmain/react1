1f9b42e53f5acc39e988e7f1f91df01253f5990f
Hlanga <henry.bani.hb@gmail.com>
2021-05-15 17:19:00 +0200

    a

--- server/index.js
+++ server/index.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const app = express();
 
+<<<<<<< Updated upstream
 app.get('/', (req, res) =>{
     res.send({hi:'there'})
 });
+=======
+passport.use(new GoogleStrategy());
+
+
+>>>>>>> Stashed changes
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
